d57619209e4dce3aff2da00a34052d914c49b2f6
Sergii Taran <srg.trn.mail@gmail.com>
2022-04-19 00:01:24 +0300

    Fix og image name

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { property: "og:title", content: "Seek & Destroy" },
       { property: "og:description", content: "Список военных преступников участвующих в войне в Украине" },
-      { property: "og:image", content:"/ogimage_b.png"}
+      { property: "og:image", content:"/ogicon_b.png"}
     ],
     script: [
       { defer: true, src: 'https://use.fontawesome.com/releases/v6.1.1/js/all.js', integrity: 'sha384-xBXmu0dk1bEoiwd71wOonQLyH+VpgR1XcDH3rtxrLww5ajNTuMvBdL5SOiFZnNdp', crossorigin: 'anonymous' },
